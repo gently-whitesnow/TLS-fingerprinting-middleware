9c3301f0cab510362714a318013bafb3c7f6f272
Александр <34779498+Canek4005@users.noreply.github.com>
2023-02-18 20:32:28 +0300

    fix deploy

--- infra/cd.bash
+++ infra/cd.bash
@@ -1,17 +1,17 @@
 # Перекладываем статику в монтируемое nginx место
-rm -rf /usr/share/nginx/html/*
-mkdir -p /usr/share/nginx/html
-cp -rf ~/build/* /usr/share/nginx/html
+# rm -rf /usr/share/nginx/html/*
+# mkdir -p /usr/share/nginx/html
+# cp -rf ~/build/* /usr/share/nginx/html
 # Перекладываем конфигурационный файл для nginx
 rm -rf /etc/nginx/*
 mkdir -p /etc/nginx
-cp -rf ~/prod/nginx.conf /etc/nginx
+cp -rf ~/TLS-fingerprinting-middleware/infra/nginx.conf /etc/nginx
 
 # Логинимся в докере, чтобы при запуске yml файла
 # была возможность скачать наш контейнер с Docker Hub
 # DOCKER_LOGIN и DOCKER_PWD/DOCKER_ACCESS_TOKEN необходимо заранее задать в 
 # environment сервера
-docker login https://index.docker.io/v2 -u $DOCKER_LOGIN -p $DOCKER_PWD 
+# docker login https://index.docker.io/v2 -u $DOCKER_LOGIN -p $DOCKER_PWD 
 
 # Запускаем сервис (перезапускаем в фоновом режиме)
 docker-compose pull
